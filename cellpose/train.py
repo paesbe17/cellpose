e05f3a1a817fd167f7a6469b585d7111ff643d46
paesbe17 <163861965+paesbe17@users.noreply.github.com>
2024-05-13 23:45:22 +0200

    Update train.py

--- cellpose/train.py
+++ cellpose/train.py
@@ -488,8 +488,8 @@ def train_seg(net, train_data=None, train_labels=None, train_files=None,
                                      p=train_probs)
         else:
             rperm = np.random.permutation(np.arange(0, nimg))
-        #for param_group in optimizer.param_groups:
-            #param_group["lr"] = LR[iepoch]
+        for param_group in optimizer.param_groups:
+            param_group["lr"] = LR[iepoch]
         net.train()
         for k in range(0, nimg_per_epoch, batch_size):
             kend = min(k + batch_size, nimg)
@@ -510,7 +510,7 @@ def train_seg(net, train_data=None, train_labels=None, train_files=None,
             loss.backward()
             optimizer.step()
             # Actualiza el scheduler
-            scheduler.step()
+            #scheduler.step()
 
             train_loss = loss.item()
             train_loss *= len(imgi)
